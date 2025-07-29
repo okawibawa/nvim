8c2f0a28c51b6b781fa2628cddb7dea83beb4063
Oka Wibawa <okaa.wibawa@gmail.com>
2025-07-29 16:39:49 +0800

    chore: updates ui

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,58 @@
 return {
   -- {
+  --   "tinted-theming/tinted-nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("tinted-colorscheme").with_config({
+  --       supports = {
+  --         tinty        = true,
+  --         tinted_shell = false,
+  --         live_reload  = true,
+  --       },
+  --       highlights = {
+  --         telescope         = true,
+  --         telescope_borders = false,
+  --         indentblankline   = true,
+  --         notify            = true,
+  --         ts_rainbow        = true,
+  --         cmp               = true,
+  --         illuminate        = true,
+  --         lsp_semantic      = true,
+  --         mini_completion   = true,
+  --         dapui             = true,
+  --       },
+  --     })
+
+  --     vim.api.nvim_create_autocmd("ColorScheme", {
+  --       callback = function()
+  --         require("lualine").setup({
+  --           options = {
+  --             theme = "auto",
+  --           },
+  --         })
+  --       end,
+  --     })
+
+  --     require("tinted-colorscheme").setup()
+  --   end,
+  -- },
+  {
+    "sainnhe/gruvbox-material",
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_background = 'medium'
+      vim.g.gruvbox_material_foreground = 'material'
+      vim.g.gruvbox_material_ui_contrast = "low"
+      vim.g.gruvbox_material_float_style = "dim"
+      vim.g.gruvbox_material_disable_italic_comment = 1
+      vim.g.gruvbox_material_enable_italic = 1
+
+      vim.cmd.colorscheme 'gruvbox-material'
+    end
+  }
+  -- {
   --   "ellisonleao/gruvbox.nvim",
   --   priority = 1000,
   --   config = function()
@@ -55,18 +108,18 @@ return {
   --     vim.cmd.colorscheme 'moonbow'
   --   end
   -- },
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    opts = {},
-    config = function()
-      require('catppuccin').setup {
-        no_italic = true,
-        no_bold = true,
-      }
+  -- {
+  --   'catppuccin/nvim',
+  --   name = 'catppuccin',
+  --   priority = 1000,
+  --   opts = {},
+  --   config = function()
+  --     require('catppuccin').setup {
+  --       no_italic = true,
+  --       no_bold = true,
+  --     }
 
-      vim.cmd.colorscheme 'catppuccin-mocha'
-    end
-  },
+  --     vim.cmd.colorscheme 'catppuccin-mocha'
+  --   end
+  -- },
 }
