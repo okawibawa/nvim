67cf96907f9f70ea538eac91706c59483c7f13cc
Oka Wibawa <okaa.wibawa@gmail.com>
2025-10-17 16:51:07 +0800

    fix: updates lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,299 +1,292 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-			"RRethy/vim-illuminate",
-			"nvim-tree/nvim-web-devicons",
-			"nvim-treesitter/nvim-treesitter",
-			"b0o/schemastore.nvim",
-			{ "mrjones2014/smart-splits.nvim", opts = {} },
-			"j-hui/fidget.nvim",
-			"nvimtools/none-ls.nvim",
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			local mason = require("mason")
-			local mason_lspconfig = require("mason-lspconfig")
-			local null_ls = require("null-ls")
+  -- Mason: Package manager
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require('mason').setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
+      })
+    end
+  },
 
-			-- Mason setup
-			mason.setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
+  -- Mason LSP bridge
+  {
+    'williamboman/mason-lspconfig.nvim',
+    dependencies = { 'williamboman/mason.nvim' },
+    config = function()
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          'ts_ls',
+          'html',
+          'cssls',
+          'tailwindcss',
+          'jsonls',
+          'gopls',
+          'pyright',
+          'lua_ls',
+          'eslint',
+        },
+        automatic_installation = true,
+      })
+    end
+  },
 
-			-- LSP servers to install
-			local servers = {
-				"ts_ls",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"jsonls",
-				"gopls",
-				"pyright",
-				"lua_ls",
-				"eslint",
-			}
+  -- Auto-install tools
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    dependencies = { 'williamboman/mason.nvim' },
+    config = function()
+      require('mason-tool-installer').setup({
+        ensure_installed = {
+          'prettierd',
+          'eslint_d',
+          'black',
+          'isort',
+        },
+        auto_update = false,
+        run_on_start = true,
+      })
+    end
+  },
 
-			-- Tools to install
-			require("mason-tool-installer").setup({
-				ensure_installed = {
-					"prettierd",
-					"eslint_d",
-					"gofumpt",
-					"goimports",
-					"black",
-					"isort",
-					"stylua",
-				},
-				auto_update = false,
-				run_on_start = true,
-			})
+  -- LSP Configuration (New API)
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'williamboman/mason-lspconfig.nvim',
+      'hrsh7th/cmp-nvim-lsp',
+    },
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-			mason_lspconfig.setup({
-				ensure_installed = servers,
-				automatic_installation = true,
-			})
+      -- IMPORTANT: Set up LspAttach BEFORE configuring servers
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+        callback = function(event)
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          local bufnr = event.buf
 
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+          if not client then return end
 
-			-- IMPORTANT: Decide which tools format which files
-			-- Prettier: JS/TS/HTML/CSS/JSON/Markdown
-			-- LSP: Go, Python, Lua
-			local prettier_formats = {
-				"javascript",
-				"javascriptreact",
-				"typescript",
-				"typescriptreact",
-				"html",
-				"css",
-				"scss",
-				"json",
-				"jsonc",
-				"markdown",
-				"yaml",
-			}
+          -- Keymaps
+          local map = function(mode, lhs, rhs, desc)
+            vim.keymap.set(mode, lhs, rhs, {
+              buffer = bufnr,
+              silent = true,
+              desc = desc or ''
+            })
+          end
 
-			-- Setup key mappings when LSP attaches
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-				callback = function(event)
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					local bufnr = event.buf
+          map('n', 'gd', vim.lsp.buf.definition, 'Go to definition')
+          map('n', 'gD', vim.lsp.buf.declaration, 'Go to declaration')
+          map('n', 'gi', vim.lsp.buf.implementation, 'Go to implementation')
+          map('n', 'gr', vim.lsp.buf.references, 'Go to references')
+          map('n', 'K', vim.lsp.buf.hover, 'Hover documentation')
+          map('n', '<leader>rn', vim.lsp.buf.rename, 'Rename')
+          map('n', '<leader>ca', vim.lsp.buf.code_action, 'Code action')
 
-					-- Disable formatting for JS/TS/HTML/CSS - let Prettier handle it
-					if
-						client
-						and vim.tbl_contains({
-							"ts_ls",
-							"html",
-							"cssls",
-							"jsonls",
-							"eslint",
-							"tailwindcss",
-						}, client.name)
-					then
-						client.server_capabilities.documentFormattingProvider = false
-					end
+          -- ESLint auto-fix
+          if client.name == 'eslint' then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.code_action({
+                  context = { only = { 'source.fixAll' }, diagnostics = {} },
+                  apply = true,
+                })
+              end,
+            })
+          end
 
-					-- Key mappings
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr })
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr })
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr })
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = bufnr })
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr })
-					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr })
-					vim.keymap.set("n", "<leader>fm", function()
-						vim.lsp.buf.format({ async = true })
-					end, { buffer = bufnr })
+          -- Document highlighting
+          if client.server_capabilities.documentHighlightProvider then
+            local group = vim.api.nvim_create_augroup('lsp-highlight-' .. bufnr, { clear = false })
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+              buffer = bufnr,
+              group = group,
+              callback = vim.lsp.buf.document_highlight,
+            })
+            vim.api.nvim_create_autocmd('CursorMoved', {
+              buffer = bufnr,
+              group = group,
+              callback = vim.lsp.buf.clear_references,
+            })
+          end
+        end,
+      })
 
-					-- ESLint auto-fix on save
-					if client and client.name == "eslint" then
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							buffer = bufnr,
-							callback = function()
-								vim.lsp.buf.code_action({
-									context = { only = { "source.fixAll.eslint" }, diagnostics = {} },
-									apply = true,
-								})
-							end,
-						})
-					end
+      -- Configure servers using new API
+      vim.lsp.config('ts_ls', {
+        capabilities = capabilities,
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = 'all',
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+            }
+          },
+          javascript = {
+            inlayHints = {
+              includeInlayParameterNameHints = 'all',
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+            }
+          }
+        }
+      })
 
-					-- Document highlight
-					if client and client.supports_method("textDocument/documentHighlight") then
-						local hl_group = vim.api.nvim_create_augroup("lsp-highlight-" .. bufnr, {})
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = bufnr,
-							group = hl_group,
-							callback = vim.lsp.buf.document_highlight,
-						})
-						vim.api.nvim_create_autocmd("CursorMoved", {
-							buffer = bufnr,
-							group = hl_group,
-							callback = vim.lsp.buf.clear_references,
-						})
-					end
-				end,
-			})
+      vim.lsp.config('html', {
+        capabilities = capabilities,
+      })
 
-			-- Configure each LSP server
-			vim.lsp.config("ts_ls", {
-				capabilities = capabilities,
-				settings = {
-					typescript = {
-						inlayHints = {
-							includeInlayParameterNameHints = "all",
-							includeInlayFunctionParameterTypeHints = true,
-							includeInlayVariableTypeHints = true,
-							includeInlayPropertyDeclarationTypeHints = true,
-							includeInlayFunctionLikeReturnTypeHints = true,
-						},
-					},
-					javascript = {
-						inlayHints = {
-							includeInlayParameterNameHints = "all",
-							includeInlayFunctionParameterTypeHints = true,
-							includeInlayVariableTypeHints = true,
-							includeInlayPropertyDeclarationTypeHints = true,
-							includeInlayFunctionLikeReturnTypeHints = true,
-						},
-					},
-				},
-			})
+      vim.lsp.config('cssls', {
+        capabilities = capabilities,
+        settings = {
+          css = { lint = { unknownAtRules = "ignore" } },
+          scss = { lint = { unknownAtRules = "ignore" } },
+          less = { lint = { unknownAtRules = "ignore" } }
+        }
+      })
 
-			vim.lsp.config("html", { capabilities = capabilities })
+      vim.lsp.config('tailwindcss', {
+        capabilities = capabilities,
+        filetypes = {
+          'html', 'css', 'scss',
+          'javascript', 'javascriptreact',
+          'typescript', 'typescriptreact'
+        },
+      })
 
-			vim.lsp.config("cssls", {
-				capabilities = capabilities,
-				settings = {
-					css = { lint = { unknownAtRules = "ignore" } },
-					scss = { lint = { unknownAtRules = "ignore" } },
-					less = { lint = { unknownAtRules = "ignore" } },
-				},
-			})
+      vim.lsp.config('jsonls', {
+        capabilities = capabilities,
+        settings = {
+          json = {
+            schemas = require('schemastore').json.schemas(),
+            validate = { enable = true },
+          },
+        },
+      })
 
-			vim.lsp.config("tailwindcss", {
-				capabilities = capabilities,
-				filetypes = {
-					"html",
-					"css",
-					"scss",
-					"javascript",
-					"javascriptreact",
-					"typescript",
-					"typescriptreact",
-				},
-			})
+      vim.lsp.config('gopls', {
+        capabilities = capabilities,
+        settings = {
+          gopls = {
+            analyses = { unusedparams = true },
+            staticcheck = true,
+            gofumpt = true,
+          },
+        },
+      })
 
-			vim.lsp.config("jsonls", {
-				capabilities = capabilities,
-				settings = {
-					json = {
-						schemas = require("schemastore").json.schemas(),
-						validate = { enable = true },
-					},
-				},
-			})
+      vim.lsp.config('pyright', {
+        capabilities = capabilities,
+      })
 
-			vim.lsp.config("gopls", {
-				capabilities = capabilities,
-				settings = {
-					gopls = {
-						analyses = { unusedparams = true },
-						staticcheck = true,
-						gofumpt = true,
-					},
-				},
-			})
+      vim.lsp.config('lua_ls', {
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = { version = 'LuaJIT' },
+            diagnostics = { globals = { 'vim' } },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = { enable = false },
+          },
+        },
+      })
 
-			vim.lsp.config("pyright", { capabilities = capabilities })
+      vim.lsp.config('eslint', {
+        capabilities = capabilities,
+      })
 
-			vim.lsp.config("lua_ls", {
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						runtime = { version = "LuaJIT" },
-						diagnostics = { globals = { "vim" } },
-						workspace = {
-							library = vim.api.nvim_get_runtime_file("", true),
-						},
-						telemetry = { enable = false },
-					},
-				},
-			})
+      -- Enable all servers
+      vim.lsp.enable({
+        'ts_ls',
+        'html',
+        'cssls',
+        'tailwindcss',
+        'jsonls',
+        'gopls',
+        'pyright',
+        'lua_ls',
+        'eslint',
+      })
 
-			vim.lsp.config("eslint", { capabilities = capabilities })
+      -- Diagnostic configuration
+      local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+      local signs_text = {}
+      for type, icon in pairs(signs) do
+        signs_text[vim.diagnostic.severity[type:upper()]] = icon
+      end
 
-			-- Enable all servers
-			vim.lsp.enable(servers)
+      vim.diagnostic.config({
+        virtual_text = { prefix = '●' },
+        signs = { text = signs_text },
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+        float = {
+          border = 'rounded',
+          source = true,
+        },
+      })
 
-			-- null-ls: Only for formatting
-			null_ls.setup({
-				sources = {
-					-- Prettier for JS/TS/HTML/CSS/JSON/etc
-					null_ls.builtins.formatting.prettierd.with({
-						filetypes = prettier_formats,
-					}),
+      -- Global diagnostic keymaps
+      vim.keymap.set('n', '<leader>de', vim.diagnostic.open_float)
+      vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1, float = true }) end)
+      vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1, float = true }) end)
+      vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist)
+    end
+  },
 
-					-- Go formatting
-					null_ls.builtins.formatting.gofumpt,
-					null_ls.builtins.formatting.goimports,
+  -- Modern formatting
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    config = function()
+      require('conform').setup({
+        formatters_by_ft = {
+          javascript = { 'prettierd' },
+          javascriptreact = { 'prettierd' },
+          typescript = { 'prettierd' },
+          typescriptreact = { 'prettierd' },
+          html = { 'prettierd' },
+          css = { 'prettierd' },
+          scss = { 'prettierd' },
+          json = { 'prettierd' },
+          jsonc = { 'prettierd' },
+          markdown = { 'prettierd' },
+          yaml = { 'prettierd' },
+          go = { 'gofumpt', 'goimports' },
+          python = { 'isort', 'black' },
+        },
 
-					-- Python formatting
-					null_ls.builtins.formatting.black,
-					null_ls.builtins.formatting.isort,
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_fallback = true,
+        },
+      })
 
-					-- Lua formatting
-					null_ls.builtins.formatting.stylua,
-				},
-			})
+      -- Manual format keymap
+      vim.keymap.set('n', '<leader>fm', function()
+        require('conform').format({ async = true, lsp_fallback = true })
+      end, { desc = 'Format buffer' })
+    end,
+  },
 
-			-- Format on save (uses null-ls for JS/TS/etc, LSP for Go/Python/Lua)
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = vim.api.nvim_create_augroup("format-on-save", { clear = true }),
-				callback = function()
-					vim.lsp.buf.format({ async = false, timeout_ms = 2000 })
-				end,
-			})
-
-			-- Diagnostic signs
-			local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-			local signs_text = {}
-			for type, icon in pairs(signs) do
-				signs_text[vim.diagnostic.severity[type:upper()]] = icon
-			end
-
-			vim.diagnostic.config({
-				virtual_text = { prefix = "●" },
-				signs = { text = signs_text },
-				underline = true,
-				update_in_insert = false,
-				severity_sort = true,
-				float = {
-					border = "rounded",
-					source = "always",
-				},
-			})
-
-			-- Diagnostic keymaps
-			vim.keymap.set("n", "<leader>de", vim.diagnostic.open_float)
-			vim.keymap.set("n", "[d", function()
-				vim.diagnostic.jump({ count = -1, float = true })
-			end)
-			vim.keymap.set("n", "]d", function()
-				vim.diagnostic.jump({ count = 1, float = true })
-			end)
-			vim.keymap.set("n", "<leader>dq", vim.diagnostic.setloclist)
-		end,
-	},
+  -- Dependencies
+  { 'b0o/schemastore.nvim' },
 }
